323c4f82582fa91fcbf6b3cd83d77352b5a5f16f
Dima Drogovoz <drogowoz@gmail.com>
2026-05-12 16:45:11 +0100

    example tool

--- lib/crewai-tools/src/crewai_tools/__init__.py
+++ lib/crewai-tools/src/crewai_tools/__init__.py
@@ -216,6 +216,7 @@ from crewai_tools.tools.youtube_video_search_tool.youtube_video_search_tool impo
     YoutubeVideoSearchTool,
 )
 from crewai_tools.tools.zapier_action_tool.zapier_action_tool import ZapierActionTools
+from crewai_tools.tools.k8s_agent_sandbox.k8s_sandbox_exec_tool import K8sSandboxExecTool
 
 
 __all__ = [
@@ -328,6 +329,7 @@ __all__ = [
     "YoutubeVideoSearchTool",
     "ZapierActionTool",
     "ZapierActionTools",
+    "K8sSandboxExecTool"
 ]
 
-__version__ = "1.14.5a4"
+__version__ = "1.14.5a3"
